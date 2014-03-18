03d5b24f2757f77700265444915128492a6c4d98
Giovane Heleno <webgeo@gmail.com>
2014-03-18 14:40:24 -0300

    Install Script

---
+++ install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+clear
+echo "Instalando ISPTools"
+cd /opt/tklweb-cp
+rm -r *
+npm install -g n
+n stable
+git init
+git pull https://giovaneh@bitbucket.org/giovaneh/isptools.git
+killall node
+npm install pm2 -g
+pm2 start app.js -i max --name ISPTools
+pm2 startup ubuntu
+reboot
