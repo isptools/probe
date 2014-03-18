e076d2e5c9550763bbbb033b8cd9244429531255
Giovane Heleno <webgeo@gmail.com>
2014-03-18 14:47:45 -0300

    no message

--- install.sh
+++ install.sh
@@ -11,4 +11,5 @@ killall node
 npm install pm2 -g
 pm2 start app.js -i max --name ISPTools
 pm2 startup ubuntu
-reboot
+
+## wget -qO- https://bitbucket.org/giovaneh/isptools/raw/master/install.sh | sh
