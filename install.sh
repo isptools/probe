6fc26961b71ac59fcc50266d4a3942641774820b
Giovane Heleno <webgeo@gmail.com>
2014-03-18 15:26:43 -0300

    no message

--- install.sh
+++ install.sh
@@ -7,18 +7,27 @@ rm -r *
 echo "OK"
 echo "Atualizando Node.js"
 npm install -g n
+echo "OK"
 n stable
 echo "OK"
 echo "Baixando ISPTools"
 git init
+echo "OK"
 git pull https://giovaneh@bitbucket.org/giovaneh/isptools.git
 echo "OK"
-echo "Iniciando Node.js"
+echo "Interrompendo instâncias Node.js fantasmas"
 killall node
+echo "OK"
+echo "Instalando PM2"
 npm install pm2 -g
+echo "OK"
+echo "Startando PM2"
 pm2 start app.js -i max --name ISPTools
+echo "OK"
+echo "Daemon PM2"
 pm2 startup ubuntu
 echo "OK"
 echo "FIM"
 
 ## wget -qO- https://bitbucket.org/giovaneh/isptools/raw/master/install.sh | sh
+
