aa3759691a1a2d29c6cce5dcda32a6ccbc39c8bf
Giovane Heleno <webgeo@gmail.com>
2014-03-19 18:51:43 -0300

    no message

--- app.js
+++ app.js
@@ -58,7 +58,7 @@ app.configure(function () {
  */
 app.get('/', function (req, res) {
     //res.redirect('http://www.isptools.com.br');
-    res.json({"err":"invalid request. check documentation.","msg": "Funcionou 12313123123123123"});
+    res.json({"err":"invalid request. check documentation.","msg": "Funcionou 1231312"});
 });
 
 /**
