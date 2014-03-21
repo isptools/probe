de8dddb703467d0118d8d4488414742bf4f3963c
Giovane Heleno <webgeo@gmail.com>
2014-03-20 22:26:48 -0300

    no message

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "Updating ISP Tools" | wall
+echo "Updating ISP Tools - www.isptools.com.br" | wall
 rm /etc/init.d/pm2-init.sh
 killall node
 pm2 kill
@@ -8,5 +8,5 @@ git fetch
 git pull origin master
 pm2 start app.js -x -f -i 1 --name ISPTools
 pm2 -f startup ubuntu
-echo "Update completed!" | wall
+echo "Update completed! - www.isptools.com.br" | wall
 echo "OK"
